da263a8d39bc7fe6ceb2e77a91f2e78aaeb2c5fc
Erica <erhu8415@colorado.edu>
2022-04-01 23:39:57 -0600

    index_update

---
+++ javascript/script.js
@@ -0,0 +1,29 @@
+var p = document.querySelector('p');
+var paragraph = document.getElementById('paragraph');
+
+p.addEventListener("mouseover", function(){
+  paragraph.style.background = "#FFC1BA"
+});
+
+p.addEventListener("mouseout", function(){
+  paragraph.style.background = "#E8AFA9"
+});
+
+var button = document.querySelector('button');
+var box = document.getElementById('changeMe');
+
+function changeColor(){
+  if(box.style.background == 'lightpink'){
+    box.style.background = 'lightblue';
+  }else{
+    box.style.background = 'lightpink';
+  }
+}
+
+// function changeColor(){
+//   if(box.style.background == 'lightpink'){
+//     box.style.background = 'lightblue';
+//   }else{
+//     box.style.background = 'lightpink';
+//   }
+// }
