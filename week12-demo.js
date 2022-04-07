9f40d42be2f472cddc5a010cb825d767363b3849
Erica <erhu8415@colorado.edu>
2022-04-07 11:44:11 -0600

    assignment12

---
+++ week12-demo.js
@@ -0,0 +1,32 @@
+// document.querySelector("button")
+// document.querySelector(".new-quote button")
+
+async function getQuote() {
+  console.log("quote button was clicked");
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    const json = await response.json();
+    console.log(json);
+    displayQuote(json.content);
+    displayAuthor(json.author);
+  }catch(err) {
+    console.log(err)
+    alert('Failed');
+  }
+}
+function displayQuote(quote) {
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+
+function displayAuthor(quote) {
+  const quoteText = document.querySelector('#author');
+  quoteText.textContent = quote;
+}
+
+const endpoint = 'http://api.quotable.io/random';
+const quoteButton = document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click', getQuote);
