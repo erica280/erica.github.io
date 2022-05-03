c6d82887fc4f8a5613720a49b227466f83ee7773
Erica <erhu8415@colorado.edu>
2022-05-03 14:18:31 -0600

    filmgen

--- javascript/filmGenerator.js
+++ javascript/filmGenerator.js
@@ -1,4 +1,4 @@
-async function getFilm() {
+async function getFilm(num) {
   console.log("quote button was clicked");
   try {
     const response = await fetch(endpoint);
@@ -7,7 +7,7 @@ async function getFilm() {
     }
     const json = await response.json();
     console.log(json);
-    displayFilm(json.Title);
+    displayFilm(json.results[num].original_title);
   }catch(err) {
     console.log(err)
     alert('Failed');
@@ -18,6 +18,13 @@ function displayFilm(film) {
   filmText.textContent = film;
 }
 
-const endpoint = 'http://www.omdbapi.com/?i=tt3896198&apikey=70cbd268';
+const endpoint = 'https://api.themoviedb.org/3/discover/movie?api_key=74d3057f71540a65a5b5bc0f2fa1d29c';
 const filmButton = document.querySelector('#js-new-film');
-filmButton.addEventListener('click', getFilm);
+getFilm(random_index);
+// var num = (Math.Floor(Math.random()*19);
+function myCallback(){
+  const random_index = (Math.floor(Math.random() * 19));
+  getFilm(random_index);
+}
+
+filmButton.addEventListener('click', myCallback);
